d69111460fa0a2011b9009dc692392af1785f275
ignaciogarcia-dev <ignaciogarcia.code@gmail.com>
2026-05-21 23:39:15 -0300

    Expose account session settings to banking

--- src/contexts/banking/domain/ports/IAccountForBankingReader.ts
+++ src/contexts/banking/domain/ports/IAccountForBankingReader.ts
@@ -1,7 +1,12 @@
+export type SessionType = 'one-shot' | 'persistent'
+export type LoginMode = 'simple' | 'assisted'
+
 export interface AccountForBanking {
   id: string
   userId: string
   bank: string
+  sessionType: SessionType
+  loginMode: LoginMode
 }
 
 export interface IAccountForBankingReader {
